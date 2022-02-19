85944f5d9a0ec45a89d3344139e1e873550fba15
Roberto Leinardi <leinardi@gmail.com>
2022-02-19 15:03:09 +0100

    Cancel future when process is canceled

--- src/main/java/com/leinardi/pycharm/pylint/util/Async.java
+++ src/main/java/com/leinardi/pycharm/pylint/util/Async.java
@@ -17,6 +17,7 @@
 package com.leinardi.pycharm.pylint.util;
 
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.progress.ProgressManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -47,7 +48,12 @@ public class Async {
 
     public static <T> Future<T> whenFinished(final Future<T> future) {
         while (!future.isDone() && !future.isCancelled()) {
-            ProgressManager.checkCanceled();
+            try {
+                ProgressManager.checkCanceled();
+            } catch (ProcessCanceledException e) {
+                future.cancel(true);
+                throw e;
+            }
             waitFor(FIFTY_MS);
         }
         return future;
